03fe1a18e97cea4b1ef88a6c5b6b5f94f143a022
Vijaykumarjntu <jntuvijaykumar@gmail.com>
2026-06-21 22:09:14 +0530

    added connection string of mongodb atlas and did resolved few connection related issues

--- backend/src/lib/db.ts
+++ backend/src/lib/db.ts
@@ -4,6 +4,11 @@ dotenv.config();
 
 export async function connectDB() {
   const uri = process.env.MONGO_URI || 'mongodb://localhost:27017/ai-travel';
-  await mongoose.connect(uri);
-  console.log('MongoDB connected');
+  // await mongoose.connect(uri);
+  // console.log('MongoDB connected');
+  // Inside your connectDB function:
+  await mongoose.connect(process.env.MONGO_URI!, {
+    family: 4 // This explicitly forces Node to connect using IPv4
+  });
+  console.log("mongodb connected with IPv4  preference");   
 }
